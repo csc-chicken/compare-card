83bf036d4d78e92e3f61d267422bbc4c36860a4b
unknown <leo.yang@cimforce.com>
2021-05-19 15:47:31 +0800

    加入專案檔案。

---
+++ 牌型比較/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 牌型比較
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
